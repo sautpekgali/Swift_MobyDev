fd255c742f296daf4fe41f1b281f8cfd0068b1f0
Gali Sautpek <142913848+sautpekgali@users.noreply.github.com>
2024-07-23 12:38:36 +0500

    Add files via upload

---
+++ Gali_Sautpek_Lab1.swift
@@ -0,0 +1,7 @@
+print("Я сегодня изучил: ")
+print("Основы языка Swift")
+print("Как использовать Playgrounds")
+
+print ("Мне нравится песня Michael Jackson:")
+print("Billie Jean ")
+print("Beat It")
