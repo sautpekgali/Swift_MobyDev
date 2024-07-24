f37e1b2c5d20f0c1a1ac647db1129179802141ee
Gali Sautpek <142913848+sautpekgali@users.noreply.github.com>
2024-07-24 15:00:42 +0500

    Add files via upload

---
+++ Gali_Sautpek_Lab2.swift
@@ -0,0 +1,44 @@
+// задание 1. Константа не может менять значение поэтому надо поменять его на переменную 
+
+var friends: Int = 216
+print (friends)
+
+friends = 184
+print (friends)
+
+// Задание 2
+
+let goalStep: Float = 10_000
+print (goalStep)
+
+/*Задание 3. Обновление константы не возможно так как она фексированна. 
+Переменная в свою очередь может быть изменина в любой момент и выведена с помощью print() команды
+*/
+
+var schooling = 12
+print (schooling)
+
+schooling = 13
+print (schooling)
+
+//Задание 4 
+var steps: Int = 0 
+var message: String
+print (steps)
+
+steps = 2000
+print (steps)
+message = "Хорошая работа! Вы уже на пути к своей ежедневной цели"
+print(message)
+//Задание 5
+var name: String 
+var errorMessage: String 
+name = "Gali Sautpek"
+print (name)
+errorMessage = " Код не работал потому что программа не видела никаких значении для переменной name"
+print (errorMessage)
+
+//Задание 6 
+var  distanceTraveled: Float = 0 
+distanceTraveled = 53.4
+print (distanceTraveled)
